97796d7d4fbb812ed283af8f595c64edd3164302
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-07-11 22:18:16 -0300

    Carpeta vs code eliminada

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d598b639884097c4463fe894b87ed80825febbf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61a9a260582a03e8d3ad0ade031023dddcb97faf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
